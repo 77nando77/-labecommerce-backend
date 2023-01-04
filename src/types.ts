e8763e3981054cfc5836fe1e0e5f75f5435772b0
luizfk <luiz.kessler7@gmail.com>
2023-01-04 16:07:29 -0300

    ecommerce

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export enum Category{
+    ACCESSORIES = "Acessórios",
+    PERIFERICS = "Periféricos",
+    ELECTRONICS = "Eletrônicos"
+}
+
+export type TUser = {
+    id: string,
+    email: string,
+    password: string
+}
+
+export type TProduct = {
+    id: string,
+    name: string,
+    price: number,
+    category: Category
+}
+
+export type TPurchase = {
+    userId: string,
+    productId: string,
+    quantity: number,
+    totalPrice: number
+}
